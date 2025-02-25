0191a804b096fc32cb8158cc61e0b1a46a27fb5d
b-c-r <bjoern.c.rall@gmail.com>
2025-02-25 13:49:52 +0100

    Added functions for model 16h

---
+++ functions_habitat_statistics/rrpe_nll_mod16h.R
@@ -0,0 +1,219 @@
+################################################################################
+#   rrpe_nll_mod16h: estimates the negative log-likelihood of mod16h           #
+#                                                                              #
+#    Copyright (C) 2025                                                        #
+#       Björn C. Rall (https://orcid.org/0000-0002-3191-8389)                  #
+#       Mireia Aranbarri (https://orcid.org/0009-0001-3506-0914)               #
+#       Lorea Flores (https://orcid.org/0000-0002-0082-4072)                   #
+#       Ioar de Guzmán (https://orcid.org/0000-0001-8894-8477)                 #
+#       Aitor Larrañaga (https://orcid.org/0000-0002-0185-9154)                #
+#       Julia Reiss (https://orcid.org/0000-0002-3740-0046)                    #
+#                                                                              #
+#    This program is free software: you can redistribute it and/or modify      #
+#    it under the terms of the GNU General Public License as published by      #
+#    the Free Software Foundation, either version 3 of the License, or         #
+#    (at your option) any later version.                                       #
+#                                                                              #
+#    This program is distributed in the hope that it will be useful,           #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#    GNU General Public License for more details.                              #
+#                                                                              #
+#    You should have received a copy of the GNU General Public License         #
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.    #
+################################################################################
+#' 
+#'
+#' @description `rrpe_nll_mod16h` calculates the negative log likelihood of
+#'     using experimental functional response data (eaten resource items as a
+#'     function of resource density) using the Michaelis-Menten Type
+#'     II functional response (Real 1977) from the `rrpe_sim` function, but
+#'     transform the values to the Holling type II (Holling 1959): T_h and a.
+#'     We calculated the likelihood by assuming a binomial distribution of the
+#'     depended data, i.e., whether a resource item can be eaten or not at the
+#'     end of the experimental trial. See (Bolker 2008), chapter 8 for details.
+#'     Moreover, the function requires the model parameters on log-scale, as
+#'     this transformation (1) accelerates the fitting procedure and (2)
+#'     prevents biologically irrelevant negative estimations that would crash
+#'     the fitting algorithm.
+#'     
+#'     Required packages and their dependencies to be installed:
+#'         - `emdbook` (Bolker 2023)
+#'     
+#'     Required packages to be attached:
+#'         - none
+#'     
+#' @references Bolker (2008) Ecological models and data in R, Princeton
+#'     University Press, Princeton, New Jersey.
+#'     https://math.mcmaster.ca/~bolker/emdbook/index.html
+#' @references Bolker (2023) emdbook: support functions and data for "Ecological
+#'     models and data". Version 1.3.13.
+#'     https://CRAN.R-project.org/package=emdbook
+#' @references Holling (1959) Some characteristics of simple types of predation
+#'     and parasitism. Can Entomol 91, 385-398.
+#'     https://doi.org/10.4039/Ent91385-7
+#' @references Real (1977) The kinetics of functional response. Am Nat 111, 289-
+#'     300. https://doi.org/10.1086/283161
+#'     
+#' @include rrpe_sim.R
+#'
+#' @param n_eaten integer (or float); the prey items that were eaten throughout the experimental trial. A vector.
+#' @param n_initial integer or float; a vector of initial prey densities.
+#' @param complexity level of complexity (0-4), a single integer value.
+#' @param p integer or float; a single value of a fixed predator density. The default value is 1.
+#' @param t_h_0_log10 t_h for the respective complexity level, single value.
+#' @param t_h_1_log10 t_h for the respective complexity level, single value.
+#' @param t_h_2_log10 t_h for the respective complexity level, single value.
+#' @param t_h_3_log10 t_h for the respective complexity level, single value.
+#' @param t_h_4_log10 t_h for the respective complexity level, single value.
+#' @param a_0_log10 a for the respective complexity level, single value.
+#' @param a_1_log10 a for the respective complexity level, single value.
+#' @param a_2_log10 a for the respective complexity level, single value.
+#' @param a_3_log10 a for the respective complexity level, single value.
+#' @param a_4_log10 a for the respective complexity level, single value.
+#' @param t_end integer or float; the time were the feeding ends. A single value; default = 1 (e.g. 1 day).
+#'
+#'     
+#' @return Returns a single negative log-likelihood value.
+#' 
+#' @examples
+#' 
+#' rm(list=ls())
+#' 
+#' library("foreach")
+#' 
+#' gh_path <- "https://raw.githubusercontent.com/b-c-r/CRITTERcode/refs/heads/main/functions_habitat_statistics/"
+#' 
+#' source(paste(gh_path, "rrpe_sim.R", sep = ""))
+#' source(paste(gh_path, "rrpe_nll_mod16h.R", sep = ""))
+#' 
+#' fr_data <- read.csv("https://raw.githubusercontent.com/b-c-r/CRITTERdata/refs/heads/main/critter_data.csv")
+#' fr_data_ie <- subset(fr_data, predator == "Ischnura elegans")
+#' 
+#' fit_ie_mod16h <- bbmle::mle2(
+#'   minuslogl = rrpe_nll_mod16h,
+#'   start = list(
+#'     t_h_0_log10  =    log10(max(fr_data_ie$n_eaten[fr_data_ie$complexity_level == 0])),
+#'     t_h_1_log10  =    log10(max(fr_data_ie$n_eaten[fr_data_ie$complexity_level == 1])),
+#'     t_h_2_log10  =    log10(max(fr_data_ie$n_eaten[fr_data_ie$complexity_level == 2])),
+#'     t_h_3_log10  =    log10(max(fr_data_ie$n_eaten[fr_data_ie$complexity_level == 3])),
+#'     t_h_4_log10  =    log10(max(fr_data_ie$n_eaten[fr_data_ie$complexity_level == 4])),
+#'     a_0_log10 = log10(mean(fr_data_ie$n_initial[fr_data_ie$complexity_level == 0])),
+#'     a_1_log10 = log10(mean(fr_data_ie$n_initial[fr_data_ie$complexity_level == 1])),
+#'     a_2_log10 = log10(mean(fr_data_ie$n_initial[fr_data_ie$complexity_level == 2])),
+#'     a_3_log10 = log10(mean(fr_data_ie$n_initial[fr_data_ie$complexity_level == 3])),
+#'     a_4_log10 = log10(mean(fr_data_ie$n_initial[fr_data_ie$complexity_level == 4]))
+#'   ),
+#'   fixed = list(
+#'     t_end = 1,
+#'     p = 1
+#'   ),
+#'   data = list(
+#'     n_eaten = fr_data_ie$n_eaten,
+#'     n_initial = fr_data_ie$n_initial,
+#'     complexity = fr_data_ie$complexity_level
+#'   ),
+#'   control = list(reltol = 1e-12),
+#'   
+#' )
+#' 
+#' bbmle::summary(fit_ie_mod16h)
+#' 
+#' #############################################################################
+#' 
+#' fr_data_ng <- subset(fr_data, predator == "Notonecta glauca")
+#' 
+#' fit_ng_mod16h <- bbmle::mle2(
+#'   minuslogl = rrpe_nll_mod16h,
+#'   start = list(
+#'     t_h_0_log10  =    log10(max(fr_data_ng$n_eaten[fr_data_ng$complexity_level == 0])),
+#'     t_h_1_log10  =    log10(max(fr_data_ng$n_eaten[fr_data_ng$complexity_level == 1])),
+#'     t_h_2_log10  =    log10(max(fr_data_ng$n_eaten[fr_data_ng$complexity_level == 2])),
+#'     t_h_3_log10  =    log10(max(fr_data_ng$n_eaten[fr_data_ng$complexity_level == 3])),
+#'     t_h_4_log10  =    log10(max(fr_data_ng$n_eaten[fr_data_ng$complexity_level == 4])),
+#'     a_0_log10 = log10(mean(fr_data_ng$n_initial[fr_data_ng$complexity_level == 0])),
+#'     a_1_log10 = log10(mean(fr_data_ng$n_initial[fr_data_ng$complexity_level == 1])),
+#'     a_2_log10 = log10(mean(fr_data_ng$n_initial[fr_data_ng$complexity_level == 2])),
+#'     a_3_log10 = log10(mean(fr_data_ng$n_initial[fr_data_ng$complexity_level == 3])),
+#'     a_4_log10 = log10(mean(fr_data_ng$n_initial[fr_data_ng$complexity_level == 4]))
+#'   ),
+#'   fixed = list(
+#'     t_end = 1,
+#'     p = 1
+#'   ),
+#'   data = list(
+#'     n_eaten = fr_data_ng$n_eaten,
+#'     n_initial = fr_data_ng$n_initial,
+#'     complexity = fr_data_ng$complexity_level
+#'   ),
+#'   control = list(reltol = 1e-12),
+#'   
+#' )
+#' 
+#' bbmle::summary(fit_ng_mod16h)
+#' 
+
+rrpe_nll_mod16h <- function(
+    n_eaten,
+    n_initial,
+    complexity,
+    p = 1,
+    t_h_0_log10,
+    t_h_1_log10,
+    t_h_2_log10,
+    t_h_3_log10,
+    t_h_4_log10,
+    a_0_log10,
+    a_1_log10,
+    a_2_log10,
+    a_3_log10,
+    a_4_log10,
+    t_end = 1
+){
+  
+  eaten_simulated <- foreach::foreach(
+    i = 1:length(n_initial),
+    .combine = "rbind"
+  ) %do% {
+    
+    if(complexity[i] == 0){
+      t_h_log10 <- t_h_0_log10
+      a_log10 <- a_0_log10
+    }
+    if(complexity[i] == 1){
+      t_h_log10 <- t_h_1_log10
+      a_log10 <- a_1_log10
+    }
+    if(complexity[i] == 2){
+      t_h_log10 <- t_h_2_log10
+      a_log10 <- a_2_log10
+    }
+    if(complexity[i] == 3){
+      t_h_log10 <- t_h_3_log10
+      a_log10 <- a_3_log10
+    }
+    if(complexity[i] == 4){
+      t_h_log10 <- t_h_4_log10
+      a_log10 <- a_4_log10
+    }
+    
+    rrpe_sim(
+      n_initial = n_initial[i],
+      p = p,
+      f_max = 1 / 10^t_h_log10,
+      a = 1 / (10^a_log10 * 10^t_h_log10),
+      t_end = t_end
+    )
+  }
+  
+  lls <- dbinom(
+    x = n_eaten,
+    size = n_initial,
+    prob = eaten_simulated$n_eaten/n_initial,
+    log = T
+    )
+  
+  nll <- -1*sum(lls)
+  
+  return(nll)
+}
---
+++ functions_habitat_statistics/rrpe_scan_mod16h.R
@@ -0,0 +1,210 @@
+################################################################################
+#   rrpe_parms_scan_mod16h: scans for reasonable starting parameters           #
+#                                                                              #
+#    Copyright (C) 2025 Björn C. Rall (https://orcid.org/0000-0002-3191-8389)  #
+#                                                                              #
+#    This program is free software: you can redistribute it and/or modify      #
+#    it under the terms of the GNU General Public License as published by      #
+#    the Free Software Foundation, either version 3 of the License, or         #
+#    (at your option) any later version.                                       #
+#                                                                              #
+#    This program is distributed in the hope that it will be useful,           #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#    GNU General Public License for more details.                              #
+#                                                                              #
+#    You should have received a copy of the GNU General Public License         #
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.    #
+################################################################################
+#'
+#' @description `rrpe_parms_scan_mod16h` creates Latin hypercube samples for the
+#'     functional response parameters in a reasonable range and calculates the
+#'     according negative log-likelihood values. It returns the parameter values
+#'     with the lowest negative log likelihood of these samples. Non-linear
+#'     maximum likelihood fitting procedures require starting parameters,
+#'     generally based on an educated guess (e.g., Bolker 2008). Moreover,
+#'     these fits may end up in local best fits, and users should re-fit the
+#'     data using different starting parameters (Bolker 2008). To overcome
+#'     manually eyeballing as well as re-shuffling the starting parameters,
+#'     Jager and Ashauer (2018) suggested creating samples in a reasonable
+#'     parameter range using and choosing the starting parameters (from the
+#'     lowest nll value) from these samples. To reduce the number of required
+#'     samples by keeping the variance of parameter values as wide as possible,
+#'     we use Latin hypercube sampling. Moreover, the function requires the
+#'     model parameters on log-scale, as this transformation (1) accelerates the
+#'     fitting procedure and (2) prevents biologically irrelevant negative
+#'     estimations that would crash the fitting algorithm.
+#'     `rrpe_parms_scan_mod16h` requires the lhs package (Carnell 2024).
+#' 
+#'     Required packages and their dependencies to be installed:
+#'       - `emdbook` (Bolker 2023)
+#'       - `foreach` (Microsoft and Weston 2022)
+#'       - `lhs` (Carnell 2024)
+#'     Required packages to be attached:
+#'       - `foreach` (Microsoft and Weston 2022)
+#' 
+#' @references Bolker (2008) Ecological models and data in R, Princeton
+#'     University Press, Princeton, New Jersey.
+#'     https://math.mcmaster.ca/~bolker/emdbook/index.html
+#' @references Bolker (2023) emdbook: support functions and data for "Ecological
+#'     models and data". Version 1.3.13.
+#'     https://CRAN.R-project.org/package=emdbook
+#' @references Carnell (2024) lhs: latin hypercube samples. Version 1.2.0.
+#'     https://CRAN.R-project.org/package=lhs
+#' @references Microsoft and Weston (2022) foreach: provides foreach looping
+#'     construct. Version 1.5.2. https://doi.org/10.32614/CRAN.package.foreach
+#' @references Jager and Ashauer (2018) Modelling survival under chemical stress
+#'     Leanpub. https://leanpub.com/guts_book
+#'
+#' @include rrpe_sim.R
+#' @include rrpe_nll_mod16h.R
+#' 
+#' @param n_eaten integer (or float); the prey items that were eaten throughout the experimental trial. A vector.
+#' @param n_initial integer or float; a vector of initial prey densities.
+#' @param complexity level of complexity (0-4), a single integer value.
+#' @param p integer or float; a single value of a fixed predator density. The def
+#' @param t_h_range_range_0_log10 A range for t_h for the respective complexity level, two values.
+#' @param t_h_range_range_1_log10 A range for t_h for the respective complexity level, two values.
+#' @param t_h_range_range_2_log10 A range for t_h for the respective complexity level, two values.
+#' @param t_h_range_range_3_log10 A range for t_h for the respective complexity level, two values.
+#' @param t_h_range_range_4_log10 A range for t_h for the respective complexity level, two values.
+#' @param a_range_range_0_log10 A range for a for the respective complexity level, two values.
+#' @param a_range_range_1_log10 A range for a for the respective complexity level, two values.
+#' @param a_range_range_2_log10 A range for a for the respective complexity level, two values.
+#' @param a_range_range_3_log10 A range for a for the respective complexity level, two values.
+#' @param a_range_range_4_log10 A range for a for the respective complexity level, two values.
+#' @param t_end integer or float; the time were the feeding ends. A single value; default = 1 (e.g. 1 day).
+#' @param no_lhs_samples a single integer value; the number of random latin hypercube samplings.
+#' 
+#' @return Returns a data frame with a single row of parameter values.
+#' 
+#' @examples
+#' 
+#' rm(list=ls())
+#' 
+#' library("foreach")
+#' 
+#' source(here::here("functions_habitat_statistics", "rrpe_sim.R"))
+#' source(here::here("functions_habitat_statistics", "rrpe_nll_mod16h.R"))
+#' source(here::here("functions_habitat_statistics", "rrpe_scan_mod16h.R"))
+#' 
+#' fr_data <- read.csv("https://raw.githubusercontent.com/b-c-r/CRITTERdata/refs/heads/main/critter_data.csv")
+#' fr_data_ie <- subset(fr_data, predator == "Ischnura elegans")
+#' 
+#' rrpe_scan_mod16h(
+#'   n_eaten = fr_data_ie$n_eaten,
+#'   n_initial = fr_data_ie$n_initial,
+#'   complexity  = fr_data_ie$complexity_level,
+#'   p = 1,
+#'   t_h_range_0_log10 = log10(c(1, max(fr_data_ie$n_eaten[fr_data_ie$complexity_level == 0]))),
+#'   t_h_range_1_log10 = log10(c(1, max(fr_data_ie$n_eaten[fr_data_ie$complexity_level == 1]))),
+#'   t_h_range_2_log10 = log10(c(1, max(fr_data_ie$n_eaten[fr_data_ie$complexity_level == 2]))),
+#'   t_h_range_3_log10 = log10(c(1, max(fr_data_ie$n_eaten[fr_data_ie$complexity_level == 3]))),
+#'   t_h_range_4_log10 = log10(c(1, max(fr_data_ie$n_eaten[fr_data_ie$complexity_level == 4]))),
+#'   a_range_0_log10 = log10(c(1, max(fr_data_ie$n_initial[fr_data_ie$complexity_level == 0]))),
+#'   a_range_1_log10 = log10(c(1, max(fr_data_ie$n_initial[fr_data_ie$complexity_level == 1]))),
+#'   a_range_2_log10 = log10(c(1, max(fr_data_ie$n_initial[fr_data_ie$complexity_level == 2]))),
+#'   a_range_3_log10 = log10(c(1, max(fr_data_ie$n_initial[fr_data_ie$complexity_level == 3]))),
+#'   a_range_4_log10 = log10(c(1, max(fr_data_ie$n_initial[fr_data_ie$complexity_level == 4]))),
+#'   t_end = 1,
+#'   no_lhs_samples = 100
+#' )
+#' 
+#' #############################################################################
+#' 
+#' fr_data_ng <- subset(fr_data, predator == "Notonecta glauca")
+#' 
+#' rrpe_scan_mod16h(
+#'   n_eaten = fr_data_ng$n_eaten,
+#'   n_initial = fr_data_ng$n_initial,
+#'   complexity  = fr_data_ng$complexity_level,
+#'   p = 1,
+#'   t_h_range_0_log10 = log10(c(1, max(fr_data_ng$n_eaten[fr_data_ng$complexity_level == 0]))),
+#'   t_h_range_1_log10 = log10(c(1, max(fr_data_ng$n_eaten[fr_data_ng$complexity_level == 1]))),
+#'   t_h_range_2_log10 = log10(c(1, max(fr_data_ng$n_eaten[fr_data_ng$complexity_level == 2]))),
+#'   t_h_range_3_log10 = log10(c(1, max(fr_data_ng$n_eaten[fr_data_ng$complexity_level == 3]))),
+#'   t_h_range_4_log10 = log10(c(1, max(fr_data_ng$n_eaten[fr_data_ng$complexity_level == 4]))),
+#'   a_range_0_log10 = log10(c(1, max(fr_data_ng$n_initial[fr_data_ng$complexity_level == 0]))),
+#'   a_range_1_log10 = log10(c(1, max(fr_data_ng$n_initial[fr_data_ng$complexity_level == 1]))),
+#'   a_range_2_log10 = log10(c(1, max(fr_data_ng$n_initial[fr_data_ng$complexity_level == 2]))),
+#'   a_range_3_log10 = log10(c(1, max(fr_data_ng$n_initial[fr_data_ng$complexity_level == 3]))),
+#'   a_range_4_log10 = log10(c(1, max(fr_data_ng$n_initial[fr_data_ng$complexity_level == 4]))),
+#'   t_end = 1,
+#'   no_lhs_samples = 100
+#' )
+#' 
+
+rrpe_scan_mod16h <- function(
+    n_eaten,
+    n_initial,
+    complexity,
+    p = 1,
+    t_h_range_0_log10,
+    t_h_range_1_log10,
+    t_h_range_2_log10,
+    t_h_range_3_log10,
+    t_h_range_4_log10,
+    a_range_0_log10,
+    a_range_1_log10,
+    a_range_2_log10,
+    a_range_3_log10,
+    a_range_4_log10,
+    t_end = 1,
+    no_lhs_samples = 1000
+){
+  
+  # generation of 
+  lhsvals <- lhs::randomLHS(no_lhs_samples, 10)
+  
+  t_h_range_0_log10   <- (lhsvals[, 1] * ( t_h_range_0_log10[2] -  t_h_range_0_log10[1])) +  t_h_range_0_log10[1]
+  t_h_range_1_log10   <- (lhsvals[, 2] * ( t_h_range_1_log10[2] -  t_h_range_1_log10[1])) +  t_h_range_1_log10[1]
+  t_h_range_2_log10   <- (lhsvals[, 3] * ( t_h_range_2_log10[2] -  t_h_range_2_log10[1])) +  t_h_range_2_log10[1]
+  t_h_range_3_log10   <- (lhsvals[, 4] * ( t_h_range_3_log10[2] -  t_h_range_3_log10[1])) +  t_h_range_3_log10[1]
+  t_h_range_4_log10   <- (lhsvals[, 5] * ( t_h_range_4_log10[2] -  t_h_range_4_log10[1])) +  t_h_range_4_log10[1]
+  a_range_0_log10  <- (lhsvals[, 6] * (a_range_0_log10[2] - a_range_0_log10[1])) + a_range_0_log10[1]
+  a_range_1_log10  <- (lhsvals[, 7] * (a_range_1_log10[2] - a_range_1_log10[1])) + a_range_1_log10[1]
+  a_range_2_log10  <- (lhsvals[, 8] * (a_range_2_log10[2] - a_range_2_log10[1])) + a_range_2_log10[1]
+  a_range_3_log10  <- (lhsvals[, 9] * (a_range_3_log10[2] - a_range_3_log10[1])) + a_range_3_log10[1]
+  a_range_4_log10  <- (lhsvals[,10] * (a_range_4_log10[2] - a_range_4_log10[1])) + a_range_4_log10[1]
+  
+  ## calculate nlls
+  nlls <- foreach::foreach(
+    i = 1:no_lhs_samples,
+    .combine = "c") %do% {
+      
+      nll <- rrpe_nll_mod16h(
+          n_eaten = n_eaten,
+          n_initial = n_initial,
+          complexity = complexity,
+          p = p,
+          t_h_0_log10 = t_h_range_0_log10[i],
+          t_h_1_log10 = t_h_range_1_log10[i],
+          t_h_2_log10 = t_h_range_2_log10[i],
+          t_h_3_log10 = t_h_range_3_log10[i],
+          t_h_4_log10 = t_h_range_4_log10[i],
+          a_0_log10 = a_range_0_log10[i],
+          a_1_log10 = a_range_1_log10[i],
+          a_2_log10 = a_range_2_log10[i],
+          a_3_log10 = a_range_3_log10[i],
+          a_4_log10 = a_range_4_log10[i],
+          t_end = t_end
+        )
+      
+    }
+  
+  sel_parms <- data.frame(
+    t_h_0_log10  =  t_h_range_0_log10[nlls == min(nlls)],
+    t_h_1_log10  =  t_h_range_1_log10[nlls == min(nlls)],
+    t_h_2_log10  =  t_h_range_2_log10[nlls == min(nlls)],
+    t_h_3_log10  =  t_h_range_3_log10[nlls == min(nlls)],
+    t_h_4_log10  =  t_h_range_4_log10[nlls == min(nlls)],
+    a_0_log10 = a_range_0_log10[nlls == min(nlls)],
+    a_1_log10 = a_range_1_log10[nlls == min(nlls)],
+    a_2_log10 = a_range_2_log10[nlls == min(nlls)],
+    a_3_log10 = a_range_3_log10[nlls == min(nlls)],
+    a_4_log10 = a_range_4_log10[nlls == min(nlls)],
+    nll = nlls[nlls == min(nlls)]
+  )
+  
+  return(sel_parms)
+}
