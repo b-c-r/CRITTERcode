2867690d48f732e1dfc072cb0794c8deaa0229de
b-c-r <bjoern.c.rall@gmail.com>
2025-03-06 18:28:26 +0100

    Added functions for habitat related statistics

---
+++ examples_habitat_statistics/mod11h_examples.R
@@ -0,0 +1,126 @@
+################################################################################
+#   Example code to fit mod11h to data                                         #
+#                                                                              #
+#    Copyright (C) 2025 Björn C. Rall (https://orcid.org/0000-0002-3191-8389)  #
+#                                                                              #
+#    This program is free software: you can redistribute it and/or modify      #
+#    it under the terms of the GNU General Public License as published by      #
+#    the Free Software Foundation, either version 3 of the License, or         #
+#    (at your option) any later version.                                       #
+#                                                                              #
+#    This program is distributed in the hope that it will be useful,           #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#    GNU General Public License for more details.                              #
+#                                                                              #
+#    You should have received a copy of the GNU General Public License         #
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.    #
+################################################################################
+#
+# find a description including here:
+#     https://github.com/b-c-r/CRITTERcode/blob/main/README.md
+#     
+# if you prefer to download a pdf, including the full statistics, follow:
+#     https://github.com/b-c-r/CRITTERstatistics/blob/main/statisticsReport.pdf
+#     
+# if you are interested in the full scientific paper follow:
+#     https://doi.org/10.1101/2025.02.22.639633
+#     
+# if you use this code, please cite:
+#     Rall et al. (2025): Habitat complexity reduces feeding strength of
+#         freshwater predators (CRITTER) - Code. Zenodo.
+#         https://doi.org/10.5281/zenodo.14894598
+#
+################################################################################
+## Setup
+
+# please install following packages, if not already done:
+
+# install.packages("bbmle")
+# install.packages("dplyr")
+# install.packages("emdbook")
+# install.packages("foreach")
+# install.packages("lhs")
+
+
+# empty environment:
+rm(list=ls())
+
+# attach libraries
+library("dplyr")
+library("foreach")
+
+# import the functions from GitHub:
+gh_path <- "https://raw.githubusercontent.com/b-c-r/CRITTERcode/refs/heads/main/"
+f_path <- "functions_habitat_statistics/"
+
+source(paste(gh_path, f_path, "rrpe_sim.R", sep = ""))                          # simulates a feeding functional response based on parameters and initial prey density
+source(paste(gh_path, f_path, "mod11h_rrpe_nll.R", sep = ""))                   # calculates negative log likelihood
+source(paste(gh_path, f_path, "mod11h_rrpe_scan.R", sep = ""))                  # calculates a set negative log likelihoods (nll) of random parameters and returns parameters from lowest nll
+source(paste(gh_path, f_path, "mod11h_rrpe_fit.R", sep = ""))                   # fits functional response model to data
+
+################################################################################
+## Data
+
+# import data from GitHub repository:
+fr_data <- read.csv(
+  "https://raw.githubusercontent.com/b-c-r/CRITTERdata/refs/heads/main/critter_data.csv"
+)
+
+# select a predator
+fr_data_ie <- subset(fr_data, predator == "Ischnura elegans")
+
+################################################################################
+## example for: mod11h_rrpe_nll.R
+
+fit_ie_mod11h <- bbmle::mle2(                                                   # mle2 - the maximum likelihood estimator from the bbmle package
+ minuslogl = mod11h_rrpe_nll,                                                   # mle2 requires the nll function for optimization
+ start = list(
+   t_h_intercept_log10  = -1.5,                                                 # start value for handling time intercept (log10 scale)
+   t_h_slope            =  0.0,                                                 # start value for handling time (linear after log10 transformation)
+   a_intercept_log10    = -1.0,                                                 # start value for attack rate intercept (log10 scale, when habitat is absent)
+   a_slope              =  0.0                                                  # start value for attack rate slope
+ ),
+ fixed = list(
+   t_end = 1,                                                                   # end time of the experiment, here 1 day
+   p = 1                                                                        # number of predators in the experiment: here 1 predator per vessel
+ ),
+ data = list(
+   n_eaten = fr_data_ie$n_eaten,                                                # data: number of prey eaten, as integer
+   n_initial = fr_data_ie$n_initial,                                            # data: number of prey provided initially, as integer
+   n_rings = fr_data_ie$ring_count                                              # data: number of habitat rings provided as structure
+ ),
+ control = list(reltol = 1e-12),                                                # fitting tolerance: the lower the better. typically around 1e-8 (see description of the optim function)
+)
+
+bbmle::summary(fit_ie_mod11h)                                                   # shows the summary table
+
+
+################################################################################
+## example for: mod11h_rrpe_scan.R
+
+mod11h_rrpe_scan(
+ n_eaten = fr_data_ie$n_eaten,                                                  # data: number of prey eaten, as integer
+ n_initial = fr_data_ie$n_initial,                                              # data: number of prey provided initially, as integer
+ n_rings = fr_data_ie$ring_count,                                               # data: number of habitat rings provided as structure
+ p = 1,                                                                         # number of predators in the experiment: here 1 predator per vessel
+ t_h_intercept_log10_range = log10(c(.05, .05)),                                # two values, the range, for handling time intercept (log10 scale)
+ t_h_slope_range = c(-0.05, 0.05),                                              # two values, the range, handling time (linear after log10 transformation)
+ a_intercept_log10_range = log10(c(.01,0.5)),                                   # two values, the range, attack rate intercept (log10 scale, when habitat is absent)
+ a_slope_range = c(-0.05,0.05),                                                 # two values, the range, attack rate slope
+ t_end = 1,                                                                     # end time of the experiment, here 1 day
+ no_lhs_samples = 100                                                           # number of latin hypercube samples that should be taken (i.e. 100 random values in the range of the above assigned ranges)
+)
+
+################################################################################
+## example for: mod11h_rrpe_fit.R
+
+mod11h_fit_ie <- mod11h_rrpe_fit(
+ n_eaten = fr_data_ie$n_eaten,                                                  # data: number of prey eaten, as integer
+ n_initial = fr_data_ie$n_initial,                                              # data: number of prey provided initially, as integer
+ n_rings = fr_data_ie$ring_count                                                # data: number of habitat rings provided as structure
+)
+
+bbmle::summary(mod11h_fit_ie)                                                   # shows the summary table of the best fit
+bbmle::AIC(mod11h_fit_ie)                                                       # shows the AIC of the best fit
+BIC(mod11h_fit_ie)                                                              # shows the BIC of the best fit
---
+++ functions_habitat_statistics/mod11h_rrpe_nll.R
@@ -0,0 +1,98 @@
+################################################################################
+#   mod11h_rrpe_nll: estimates the negative log-likelihood                     #
+#                                                                              #
+#    Copyright (C) 2025                                                        #
+#       Björn C. Rall (https://orcid.org/0000-0002-3191-8389)                  #
+#       Mireia Aranbarri (https://orcid.org/0009-0001-3506-0914)               #
+#       Lorea Flores (https://orcid.org/0000-0002-0082-4072)                   #
+#       Ioar de Guzmán (https://orcid.org/0000-0001-8894-8477)                 #
+#       Aitor Larrañaga (https://orcid.org/0000-0002-0185-9154)                #
+#       Julia Reiss (https://orcid.org/0000-0002-3740-0046)                    #
+#                                                                              #
+#    This program is free software: you can redistribute it and/or modify      #
+#    it under the terms of the GNU General Public License as published by      #
+#    the Free Software Foundation, either version 3 of the License, or         #
+#    (at your option) any later version.                                       #
+#                                                                              #
+#    This program is distributed in the hope that it will be useful,           #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#    GNU General Public License for more details.                              #
+#                                                                              #
+#    You should have received a copy of the GNU General Public License         #
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.    #
+################################################################################
+#' 
+#' @description
+#'     find the description including parameters here:
+#'         https://github.com/b-c-r/CRITTERcode/blob/main/README.md
+#'     
+#'     if you prefer to download a pdf, including the full statistics, follow:
+#'         https://github.com/b-c-r/CRITTERstatistics/blob/main/statisticsReport.pdf
+#'     
+#'     if you are interested in the full scientific paper follow:
+#'         https://doi.org/10.1101/2025.02.22.639633
+#'     
+#'     if you use this code, please cite:
+#'         Rall et al. (2025): Habitat complexity reduces feeding strength of
+#'         freshwater predators (CRITTER) - Code. Zenodo.
+#'         https://doi.org/10.5281/zenodo.14894598
+#' 
+#'      please also consider citing for the underlying method:
+#'         Bolker (2008) Ecological models and data in R, Princeton University Press,
+#'         Princeton, New Jersey.
+#'         https://math.mcmaster.ca/~bolker/emdbook/index.html
+#' 
+#' @include rrpe_sim.R
+#' 
+#' @return Returns a single negative log-likelihood value.
+#' 
+#' @examples
+#' 
+#' # find an executable example here:
+#' # https://github.com/b-c-r/CRITTERcode/examples_habitat_statistics/examples_habitat_statistics/mod11h_examples.R
+#' 
+
+mod11h_rrpe_nll <- function(
+    n_eaten,
+    n_initial,
+    n_rings,
+    p = 1,
+    t_h_intercept_log10,
+    t_h_slope,
+    a_intercept_log10,
+    a_slope,
+    t_end = 1
+){
+  
+  nll <- Inf
+  
+  try({
+    eaten_simulated <- foreach::foreach(
+      i = 1:length(n_initial),
+      .combine = "rbind"
+    ) %do% {
+      
+      rrpe_sim(
+        fr_style = "Holling",
+        n_initial = n_initial[i],
+        p = p,
+        a = 10^(a_intercept_log10 + a_slope * n_rings[i]),
+        t_h = 10^(t_h_intercept_log10 + t_h_slope * n_rings[i]),
+        t_end = t_end
+      )
+    }
+    
+    lls <- dbinom(
+      x = n_eaten,
+      size = n_initial,
+      prob = eaten_simulated$n_eaten/n_initial,
+      log = T
+    )
+    
+    nll <- -1*sum(lls)
+    
+  }, silent = TRUE)
+  
+  return(nll)
+}
