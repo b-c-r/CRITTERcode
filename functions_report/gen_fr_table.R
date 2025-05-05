bd3dde393aad94d13bbc41b0bbaef19d5e2bf5d6
b-c-r <bjoern.rall@uni-konstanz.de>
2025-05-05 20:51:26 +0200

    Correct typo

--- functions_report/gen_fr_table.R
+++ functions_report/gen_fr_table.R
@@ -16,7 +16,7 @@
 #    You should have received a copy of the GNU General Public License         #
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.    #
 ################################################################################
-' 
+#' 
 #' @description
 #'     find the description including parameters here:
 #'         https://github.com/b-c-r/CRITTERcode/blob/main/README.md
